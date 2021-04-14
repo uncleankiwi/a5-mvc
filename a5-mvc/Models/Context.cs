2557de43f199c2d05e24af112ea50caf8029d57c
uncleankiwi <feldtspirit@gmail.com>
2021-04-14 02:47:15 -0400

    -

---
+++ a5-mvc/Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace a5_mvc.Models
+{
+	public class Context : DbContext
+	{
+		DbSet<Pet> Pets { get; set; }
+		DbSet<SoldPet> SoldPets { get; set; }
+		DbSet<Category> Categories { get; set; }
+		DbSet<Customer> Customers { get; set; }
+		DbSet<Sale> Sales { get; set; }
+	}
+}
