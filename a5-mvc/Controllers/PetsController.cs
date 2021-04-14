1ba9b55dd13ad5cf5d1304177f4cbe85dff4c5d5
uncleankiwi <feldtspirit@gmail.com>
2021-04-14 17:39:50 -0400

    -

--- a5-mvc/Controllers/PetsController.cs
+++ a5-mvc/Controllers/PetsController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace a5_mvc.Controllers
 {
@@ -21,13 +22,13 @@ namespace a5_mvc.Controllers
 		// GET: Pets/Details/5
 		public ActionResult Details(int id)
 		{
-			return View(ctx.Pets.Find(id));
+			return View(PetFromId(id));
 		}
 
 		// GET: Pets/Create
 		public ActionResult Create()
 		{
-			return View();
+			return View(InitDDL(new Pet()));
 		}
 
 		// POST: Pets/Create
@@ -49,7 +50,7 @@ namespace a5_mvc.Controllers
 		// GET: Pets/Edit/5
 		public ActionResult Edit(int id)
 		{
-			return View(ctx.Pets.Find(id));
+			return View(InitDDL(PetFromId(id)));
 		}
 
 		// POST: Pets/Edit/5
@@ -71,7 +72,7 @@ namespace a5_mvc.Controllers
 		// GET: Pets/Delete/5
 		public ActionResult Delete(int id)
 		{
-			return View(ctx.Pets.Find(id));
+			return View(PetFromId(id));
 		}
 
 		// POST: Pets/Delete/5
@@ -91,9 +92,16 @@ namespace a5_mvc.Controllers
 		}
 
 		/////////////////////utility methods
-		private void InitDDL(Pet pet)
+		private Pet PetFromId (int id)
+		{
+			return ctx.Pets
+				.Include(x => x.Category)
+				.Single(x => x.Id == id);
+		}
+		private Pet InitDDL(Pet pet)
 		{
 			pet.CategoriesList = App.GetCategoriesDDL();
+			return pet;
 		}
 	}
 }
--- a5-mvc/Controllers/SalesController.cs
+++ a5-mvc/Controllers/SalesController.cs
@@ -5,95 +5,104 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace a5_mvc.Controllers
 {
-    public class SalesController : Controller
-    {
-        Context ctx = new Context();
-        // GET: Sales
-        public ActionResult Index()
-        {
-            return View(ctx.Sales.ToList());
-        }
-
-        // GET: Sales/Details/5
-        public ActionResult Details(int id)
-        {
-            return View(ctx.SoldPets.Find(id));
-        }
-
-        // GET: Sales/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Sales/Create
-        [HttpPost]
-        public ActionResult Create(Sale sale)
-        {
-            try
-            {
-                ctx.Sales.Add(sale);
-                ctx.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //// GET: Sales/Edit/5
-        //public ActionResult Edit(int id)
-        //{
-        //    return View(ctx.Sales.Find(id));
-        //}
-
-        //// POST: Sales/Edit/5
-        //[HttpPost]
-        //public ActionResult Edit(Sale sale)
-        //{
-        //    try
-        //    {
-                
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-
-        // GET: Sales/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View(ctx.Sales.Find(id));
-        }
-
-        // POST: Sales/Delete/5
-        [HttpPost]
-        public ActionResult Delete(Sale sale)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        ////////////////////utility methods
-        private void InitDDL(Sale sale)
+	public class SalesController : Controller
+	{
+		Context ctx = new Context();
+		// GET: Sales
+		public ActionResult Index()
 		{
-            sale.CustomerList = App.GetCustomersDDL();
-            sale.PetsList = App.GetPetsDDL();
+			return View(ctx.Sales.ToList());
 		}
 
-    }
+		// GET: Sales/Details/5
+		public ActionResult Details(int id)
+		{
+			return View(ctx.SoldPets.Find(id));
+		}
+
+		// GET: Sales/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: Sales/Create
+		[HttpPost]
+		public ActionResult Create(Sale sale)
+		{
+			try
+			{
+				ctx.Sales.Add(sale);
+				ctx.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		//// GET: Sales/Edit/5
+		//public ActionResult Edit(int id)
+		//{
+		//    return View(ctx.Sales.Find(id));
+		//}
+
+		//// POST: Sales/Edit/5
+		//[HttpPost]
+		//public ActionResult Edit(Sale sale)
+		//{
+		//    try
+		//    {
+				
+		//        return RedirectToAction("Index");
+		//    }
+		//    catch
+		//    {
+		//        return View();
+		//    }
+		//}
+
+		// GET: Sales/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View(ctx.Sales.Find(id));
+		}
+
+		// POST: Sales/Delete/5
+		[HttpPost]
+		public ActionResult Delete(Sale sale)
+		{
+			try
+			{
+				// TODO: Add delete logic here
+
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		////////////////////utility methods
+		private Sale SaleFromId(int id)
+		{
+			return ctx.Sales
+				.Include(x => x.Customer)
+				.Include(x => x.SoldPet)
+				.Single(x => x.Id == id);
+		}
+		private Sale InitDDL(Sale sale)
+		{
+			sale.CustomerList = App.GetCustomersDDL();
+			sale.PetsList = App.GetPetsDDL();
+			return sale;
+		}
+
+	}
 }
