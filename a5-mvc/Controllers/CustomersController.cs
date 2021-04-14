6d300ee311e86a18873aa0ed941d116a380a60c5
uncleankiwi <feldtspirit@gmail.com>
2021-04-14 13:48:22 -0400

    -

--- a5-mvc/Controllers/CustomersController.cs
+++ a5-mvc/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using a5_mvc.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,84 +7,85 @@ using System.Web.Mvc;
 
 namespace a5_mvc.Controllers
 {
-    public class CustomersController : Controller
-    {
-        // GET: Customers
-        public ActionResult Index()
-        {
-            return View();
-        }
+	public class CustomersController : Controller
+	{
+		Context ctx = new Context();
+		// GET: Customers
+		public ActionResult Index()
+		{
+			return View(ctx.Customers.ToList());
+		}
 
-        // GET: Customers/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+		// GET: Customers/Details/5
+		public ActionResult Details(int id)
+		{
+			return View(ctx.Customers.Find(id));
+		}
 
-        // GET: Customers/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+		// GET: Customers/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
 
-        // POST: Customers/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
+		// POST: Customers/Create
+		[HttpPost]
+		public ActionResult Create(Customer customer)
+		{
+			try
+			{
+				ctx.Customers.Add(customer);
+				ctx.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+		// GET: Customers/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View(ctx.Customers.Find(id));
+		}
 
-        // GET: Customers/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+		// POST: Customers/Edit/5
+		[HttpPost]
+		public ActionResult Edit(Customer customer)
+		{
+			try
+			{
+				ctx.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+				ctx.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
 
-        // POST: Customers/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
+		// GET: Customers/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View(ctx.Customers.Find(id));
+		}
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Customers/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Customers/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-    }
+		// POST: Customers/Delete/5
+		[HttpPost]
+		public ActionResult Delete(Customer customer)
+		{
+			try
+			{
+				ctx.Customers.Remove(customer);
+				ctx.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
 }
